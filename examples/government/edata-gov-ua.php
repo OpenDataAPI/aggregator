05c283260ec7d7c121b62ce86e0fd2bb7ee54441
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-07-06 10:25:52 +0300

    edata.gov.ua API wrapper usage example added.

---
+++ examples/government/edata-gov-ua.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8">
+    </head>
+    <body>
+<?php
+
+require_once("../../vendor/autoload.php");
+
+use OpenDataAPI\aggregator\providers\government\EDataGovUA;
+
+$transactions = EDataGovUA::getTransactions([
+    "startdate" => "01-12-2015",
+    "enddate" => "01-02-2016",
+    "regions" => [5], // Regions IDs from GeoData::$regions
+    "payers_edrpous" => ["39883094","09334702"],
+    "recipt_edrpous" => ["09334702","39883094"]
+]);
+
+foreach ($transactions->response->transactions as $transaction) {
+    var_dump($transaction);
+}
+
+?>
+    </body>
+</html>
