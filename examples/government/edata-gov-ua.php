ed2314b090fbc8b63a261ea78524d18ff2c79f52
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-07-06 10:49:14 +0300

    edata.gov.ua API wrapper PHP Docs Ukrainian translation added.

--- examples/government/edata-gov-ua.php
+++ examples/government/edata-gov-ua.php
@@ -13,7 +13,7 @@ use OpenDataAPI\aggregator\providers\government\EDataGovUA;
 $transactions = EDataGovUA::getTransactions([
     "startdate" => "01-12-2015",
     "enddate" => "01-02-2016",
-    "regions" => [5], // Regions IDs from GeoData::$regions
+    "regions" => [5], // Ідентифікатор регіону з GeoData::$regions (Regions IDs from GeoData::$regions)
     "payers_edrpous" => ["39883094","09334702"],
     "recipt_edrpous" => ["09334702","39883094"]
 ]);
--- src/providers/government/EDataGovUA.php
+++ src/providers/government/EDataGovUA.php
@@ -5,8 +5,11 @@ namespace OpenDataAPI\aggregator\providers\government;
 use OpenDataAPI\aggregator\tools\Request;
 
 /**
+ * Функціонал для роботи з API http://edata.gov.ua/.
+ * Приклади використання у папці /examples/government.
+ *
  * http://edata.gov.ua/ API wrapper.
- * Usage in the examples folder.
+ * Usage in the /examples/government folder.
  *
  * @category Open Data APIs Aggregator
  * @author Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
@@ -20,9 +23,11 @@ class EDataGovUA {
     const URL = "http://api.e-data.gov.ua:8080/api/rest/1.0/"; // 2M requests / day
 
     /**
+     * Повертає список транзакцій за заданими параметрами.
+     *
      * Returns transactions list by parameters in JSON.
      *
-     * @param array $json Data array for JSON.
+     * @param array $json Массив данних для формування JSON (Data array for JSON).
      *
      * @return object JSON stdClass object.
      */
