b882a1d2c8ca298e05339fa6cc996c89e157e9ba
Dmytro P. Zarezenko <dmytro.zarezenko@gmail.com>
2016-07-11 17:29:50 +0300

    Examples index added.

---
+++ examples/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Open Data APIs Aggregator</h1>
+
+        GitHub: <a href="https://github.com/OpenDataAPI/aggregator">https://github.com/OpenDataAPI/aggregator</a>
+
+        <ul>
+            <li>
+                <strong>Government</strong>
+                <ol>
+                    <li><a href="/government/edata-gov-ua.php">edata.gov.ua</a></li>
+                    <li><a href="/government/data-gov-ua.php">data.gov.ua</a></li>
+                    <li><a href="/government/data-rada-gov-ua.php">data.rada.gov.ua</a></li>
+                    <li><a href="/government/bank-gov-ua.php">bank.gov.ua</a></li>
+                </ol>
+            </li>
+            <li>
+                <strong>Services</strong>
+                <ol>
+                    <li><a href="/services/donor-ua.php">donor.ua</a></li>
+                </ol>
+            </li>
+            <li>
+                <strong>Banks</strong>
+            </li>
+        </ul>
+
+        Copyright &copy; 2016 OpenDataAPI Aggregator by Dmytro Zarezenko
+    </body>
+</html>
